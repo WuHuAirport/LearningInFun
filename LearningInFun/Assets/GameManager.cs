8161450304b70ca275a87d1fa4a910ec20105925
浩杰 谢 <793048004@qq.com>
2020-05-16 16:35:22 +0800

    基本框架

---
+++ LearningInFun/Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum SceneTypes
+{
+    startScene,
+
+}
+
+public class GameManager : Singleton<GameManager>
+{
+    //软件逻辑方面
+    private int sceneNumber;
+
+
+    //游戏方面
+    private int wordsNumber; //每波单词数
+    private int reactionSeconds;//反应秒数
+    private int times;      //波数
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnterWordUsedScene()
+    {
+        SceneMgr.GetInstance().LoadSceneAsyn("Scenes/WordUsedScene");
+    }
+
+
+    /// <summary>
+    /// 退出整个软件
+    /// </summary>
+    public void QuitSoftware()
+    {
+#if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+        
+#else
+         Application.Quit();
+#endif
+    }
+}
---
+++ LearningInFun/Assets/Scripts/Basic/BaseManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//使用方法：需要单例的直接继承即可
+public class BaseManager<T> where T:new ()
+{
+    private static T instance;
+    public static T GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new T();
+        }
+        return instance;
+    }
+}
