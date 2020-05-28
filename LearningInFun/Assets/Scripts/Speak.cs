e09b8a8d32a0b5f97524a0a86b025ec17ed22bc2
浩杰 谢 <793048004@qq.com>
2020-05-28 23:38:04 +0800

    讲述人，开始界面

---
+++ LearningInFun/Assets/Scripts/Speak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+using UnityEngine.Windows.Speech;
+
+public class Speak : MonoBehaviour
+{
+    public string[] keyWords = new string[] { "确认", "开始", "返回", "暂停" };
+    public ConfidenceLevel confidenLevel = ConfidenceLevel.Medium;
+    PhraseRecognizer recognizer;
+    void Start()
+    {
+        recognizer = new KeywordRecognizer(keyWords, confidenLevel);
+        recognizer.OnPhraseRecognized += Display;  // 注册事件  
+        recognizer.Start();
+    }
+
+    public void Display(PhraseRecognizedEventArgs args)
+    {
+        string str = args.text;
+        Debug.Log(str.ToString());
+    }
+}  
+
---
+++ LearningInFun/Assets/Scripts/SpeechTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeechLib;
+using System.Threading;
+
+public class SpeechTest : MonoBehaviour
+{
+
+    Thread t;
+    SpVoice spVoice;
+    string DefaultEnglishLangID = "804";//中文 409：英文
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            t = null;
+            SpeakVoice("哈哈，正义必胜，hello world");
+        }
+    }
+
+    public void SpeakVoice(string content)
+    {
+        try
+        {
+            if (t == null)
+            {
+                t = new Thread(() =>
+                {
+                    string contentStr = "<voice required=\"Language=" + DefaultEnglishLangID + "\">" + content + "</voice>";
+
+                    if (spVoice == null)
+                    {
+                        spVoice = new SpVoice();
+                        //spVoice.Voice = spVoice.GetVoices(string.Empty, string.Empty).Item(0);
+                        spVoice.Speak(contentStr, SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak | SpeechVoiceSpeakFlags.SVSFlagsAsync);
+                    }
+                    else
+                    {
+                        spVoice.Speak(contentStr, SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak | SpeechVoiceSpeakFlags.SVSFlagsAsync);
+                    }
+                });
+            }
+            t.Start();
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
+
